457ce9deb5c02130160682249c795abde20ba50d
Felipe Simplício Ferreira <felipe.ferreira@ipea.gov.br>
2020-11-10 12:13:33 -0300

    Atualização do código
    Atualmente não funcionando em versões mais recentes do R e o pacote "eurostat" saiu do CRAN. Adicionado exportação em formato xlsx.

--- Dados Euro.R
+++ Dados Euro.R
@@ -1,14 +1,16 @@
 #Rotina para coletar séries do eurostat
 #Feito por: Felipe Simplício Ferreira
-#última atualização: 24/11/2019
+#última atualização: 10/11/2020
 
 #Definindo diretórios a serem utilizados
 getwd()
 setwd("C:/Users/User/Documents")
 
 #Carregando pacotes que serão utilizados
-library("dplyr")
-library("eurostat")
+library(dplyr)
+library(devtools)
+#install_github("ropengov/eurostat")
+library(eurostat)
 
 #1)PIB EURO
 pib = get_eurostat("namq_10_gdp")
@@ -18,6 +20,7 @@ pib_AE = subset(pib_AE, select = c(time, values))
 colnames(pib_AE) = c('data', "Euro area (EA11-2000, EA12-2006, EA13-2007, EA15-2008, EA16-2010, EA17-2013, EA18-2014, EA19)")
 
 write.csv2(pib_AE,"01-pib_AE.csv", row.names = F)
+export(pib_AE, "dados_euro.xlsx", sheetName = "pib_AE")
 
 #2)PIB países
 codigo_paises = c("DE", "EL", "FR", "IT", "UK", "PT", "ES")
@@ -41,6 +44,7 @@ for (i in 1:length(codigo_paises)){
 }
 
 write.csv2(pib_paises,"02-pib_paises.csv", row.names = F)
+export(pib_paises, "dados_euro.xlsx", which = "pib_paises")
 
 #3) Tabelas var. PIB
 coleta_tabelas = function(codigo_itens, unidade, codigo_saz, nome_itens){
@@ -63,10 +67,12 @@ nome_itens1 = c("PIB", "Consumo das famílias", "FBKF", "Variação de estoques", "
 #Zona do Euro - PIB - Contribuição para a Variação Trimestral,  ante ao período anterior, com ajuste sazonal
 tabela1 = coleta_tabelas(codigo_itens1, "CON_PPCH_PRE", "SCA", nome_itens1)
 write.csv2(tabela1,"03-tabela1.csv", row.names = F)
+export(tabela1, "dados_euro.xlsx", which = "tabela1")
 
 #Zona do Euro - PIB - Contribuição para a variação ante igual período do ano anterior, sem ajuste sazonal  (%)
 tabela2 = coleta_tabelas(codigo_itens1, "CON_PPCH_SM", "NSA", nome_itens1)
 write.csv2(tabela2,"03-tabela2.csv", row.names = F)
+export(tabela2, "dados_euro.xlsx", which = "tabela2")
 
 
 codigo_itens2 = c("B1GQ", "P31_S14_S15", "P51G", "P3_S13", "P6", "P7")
@@ -75,12 +81,12 @@ nome_itens2 = c("PIB", "Consumo das famílias", "FBKF", "Gastos do Governo", "Exp
 #Zona do Euro - PIB - Variação Trimestral,  ante ao período anterior, com ajuste sazonal  (%)
 tabela3 = coleta_tabelas(codigo_itens2, "CLV_PCH_PRE", "SCA", nome_itens2)
 write.csv2(tabela3,"03-tabela3.csv", row.names = F)
-
+export(tabela3, "dados_euro.xlsx", which = "tabela3")
 
 #Zona do Euro - PIB - Variação Trimestral,  ante igual período do ano anterior, sem ajuste sazonal  (%)
 tabela4 = coleta_tabelas(codigo_itens2, "CLV_PCH_SM", "NSA", nome_itens2)
 write.csv2(tabela4,"03-tabela4.csv", row.names = F)
-
+export(tabela4, "dados_euro.xlsx", which = "tabela4")
 
 #4) Inflação
 inflacao = get_eurostat("prc_hicp_manr")
@@ -100,6 +106,7 @@ colnames(inflacao_nucleo) = c('data', "Overall index excluding energy, food, alc
 inflacao_merge = merge(inflacao_tot, inflacao_nucleo, by = "data", all = T)
 
 write.csv2(inflacao_merge,"04-inflacao.csv", row.names = F)
+export(inflacao_merge, "dados_euro.xlsx", which = "inflacao")
 
 #5) Produção industrial mensal
 producao_industrial_mensal = get_eurostat("sts_inpr_m")
@@ -125,6 +132,7 @@ producao_industrial_mensal_merge = merge(producao_industrial_mensal_total, produ
 producao_industrial_mensal_merge = merge(producao_industrial_mensal_merge, producao_industrial_mensal_eletricidade, by = "data", all = T)
 
 write.csv2(producao_industrial_mensal_merge,"05-producao_industrial_mensal.csv", row.names = F)
+export(producao_industrial_mensal_merge, "dados_euro.xlsx", which = "producao industrial mensal")
 
 
 #6) Produção industrial trimestral
@@ -151,6 +159,7 @@ producao_industrial_trimestral_merge = merge(producao_industrial_trimestral_tota
 producao_industrial_trimestral_merge = merge(producao_industrial_trimestral_merge, producao_industrial_trimestral_eletricidade, by = "data", all = T)
 
 write.csv2(producao_industrial_trimestral_merge,"06-producao_industrial_trimestral.csv", row.names = F)
+export(producao_industrial_trimestral_merge, "dados_euro.xlsx", which = "producao industrial trimestral")
 
 
 #6)Vendas no varejo
@@ -161,3 +170,4 @@ vendas_varejo_filtrado = subset(vendas_varejo_filtrado, select = c(time, values)
 colnames(vendas_varejo_filtrado) = c('data', "Retail trade, except of motor vehicles and motorcycles")
 
 write.csv2(vendas_varejo_filtrado,"07-vendas_varejo.csv", row.names = F)
+export(vendas_varejo_filtrado, "dados_euro.xlsx", which = "vendas_varejo")
